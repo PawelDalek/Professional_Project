6ab7e05bfc4acc939db19fa6dc2c52e2b516441d
PawelDalek <pawel.piotr.dalek@gmail.com>
2022-10-13 15:56:50 +0200

    Init

---
+++ src/main/java/com/learning/Professional_Project/ProfessionalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.learning.Professional_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/Professional_Project/ProfessionalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.Professional_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
